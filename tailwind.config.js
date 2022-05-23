3fa0abf33dbd33fd6646fe422d425b364b1f03ea
Glenn de Haan <glenn@dehaan.cloud>
2022-05-23 11:45:23 +0200

    Implemented more service page elements. Also updated the tailwind breakpoint to better accommodate the new service page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,22 @@ module.exports = {
     content: ["./src/template/**/*.{html,js,ejs}"],
     darkMode: 'media',
     theme: {
+        screens: {
+            'sm': '640px',
+            // => @media (min-width: 640px) { ... }
+
+            'md': '768px',
+            // => @media (min-width: 768px) { ... }
+
+            'lg': '1124px',
+            // => @media (min-width: 1124px) { ... }
+
+            'xl': '1280px',
+            // => @media (min-width: 1280px) { ... }
+
+            '2xl': '1536px',
+            // => @media (min-width: 1536px) { ... }
+        },
         extend: {}
     },
     variants: {
