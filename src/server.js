d5af8cdff0f9aa9ebaebf5b10307aa90965ff443
Glenn de Haan <glenn@dehaan.cloud>
2022-05-25 11:16:05 +0200

    Implemented the download task logs feature

--- src/server.js
+++ src/server.js
@@ -247,6 +247,36 @@ app.get('/service/:service/task/:task', async (req, res) => {
     });
 });
 
+app.get('/service/:service/task/:task/logs/download', async (req, res) => {
+    const service = await docker.getService(req.params.service);
+
+    if(typeof service.Spec === "undefined") {
+        res.status(404);
+        res.render('404', {
+            ...await pageVariables(req, db),
+            page_title: `Not Found`
+        });
+        return;
+    }
+
+    const task = await docker.getTask(req.params.task);
+
+    if(typeof task.Spec === "undefined") {
+        res.status(404);
+        res.render('404', {
+            ...await pageVariables(req, db),
+            page_title: `Not Found`
+        });
+        return;
+    }
+
+    const logs = await docker.getTaskLogs(req.params.task);
+    const task_logs = logs.length > 0 ? demux(logs).join('') : '1:M 04 May 2022 09:26:00.642 # WARNING overcommit_memory is set to 0! Background save may fail under low memory condition. To fix this issue add \'vm.overcommit_memory = 1\' to /etc/sysctl.conf and then reboot or run the command \'sysctl vm.overcommit_memory=1\' for this to take effect.';
+
+    res.set('Content-Type', 'text/plain');
+    res.send(task_logs);
+});
+
 app.get('/update/:service', async (req, res) => {
     const service = await docker.getService(req.params.service);
 
