cd81074502b3e3300738bcc687cec02351b0a460
Glenn de Haan <glenn@dehaan.cloud>
2022-06-14 11:38:24 +0200

    Implemented the data call to ship

--- agent/src/server.js
+++ agent/src/server.js
@@ -1,4 +1,9 @@
 /**
+ * Import vendor modules
+ */
+const fetch = require('node-fetch');
+
+/**
  * Import own modules
  */
 const log = require('./modules/logger');
@@ -45,6 +50,7 @@ const update = async () => {
     log.info(`[UPDATE] Started at: ${new Date()}`);
 
     const data = {};
+    const dockerInfo = await docker.info();
     const containers = await docker.getContainers();
 
     log.info(`[UPDATE] Found ${containers.length} container(s)`);
@@ -58,6 +64,21 @@ const update = async () => {
         data[containers[item].Id].stats = stats;
         data[containers[item].Id].processes = processes;
     }
+
+    log.info('[UPDATE] Attempting to send data to Ship...');
+
+    await fetch(`http://ship:3000/agent/${dockerInfo.ID}`, {
+        method: 'post',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(() => {
+        log.info(`[UPDATE] Data send to Ship!`);
+    }).catch((e) => {
+        log.error('[UPDATE] Unable to send data to ship! Error Message:')
+        log.error(e);
+    });
 
     log.info(`[UPDATE] Completed at: ${new Date()}`);
     log.info(`[UPDATE] Next update at: ${new Date(nextUpdate)}`);
