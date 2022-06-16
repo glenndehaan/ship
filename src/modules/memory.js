93150b234091c333b426190f66cd39edd0ca2ae2
Glenn de Haan <glenn@dehaan.cloud>
2022-06-16 13:04:51 +0200

    Added the ship agent network configuration to the task page

--- src/modules/memory.js
+++ src/modules/memory.js
@@ -12,6 +12,58 @@ const mockData = {
     agent: {
         example_node: {
             "00000000000000000": {
+                info: {
+                    Id: "85f6f97dd19b4e5895edfdb7fc0a5d008783cd6c162e0b24de15f5c8cd4dfad4",
+                    Names: [
+                        "/whaomi_whoami.3.yk5ou107d75fe6hi7w8w4xob9"
+                    ],
+                    Image: "containous/whoami:latest@sha256:7d6a3c8f91470a23ef380320609ee6e69ac68d20bc804f3a1c6065fb56cfa34e",
+                    ImageID: "sha256:072fd1aacecbccbdea9862bbe1d655e7d089103cb1698b216f4bf06ccec31097",
+                    Command: "/whoami",
+                    Created: 1655208262,
+                    Ports: [
+                        {
+                            PrivatePort: 80,
+                            Type: "tcp"
+                        }
+                    ],
+                    Labels: {
+                        "com.docker.stack.namespace": "whaomi",
+                        "com.docker.swarm.node.id": "5k0xw1l76tg0hp53gyqdjlxua",
+                        "com.docker.swarm.service.id": "xf486jy6c4kkv6n5vo322nxbg",
+                        "com.docker.swarm.service.name": "whaomi_whoami",
+                        "com.docker.swarm.task": "",
+                        "com.docker.swarm.task.id": "yk5ou107d75fe6hi7w8w4xob9",
+                        "com.docker.swarm.task.name": "whaomi_whoami.3.yk5ou107d75fe6hi7w8w4xob9"
+                    },
+                    State: "running",
+                    Status: "Up 45 hours",
+                    HostConfig: {
+                        NetworkMode: "default"
+                    },
+                    NetworkSettings: {
+                        Networks: {
+                            whaomi_default: {
+                                IPAMConfig: {
+                                    IPv4Address: "10.0.1.6"
+                                },
+                                Links: null,
+                                Aliases: null,
+                                NetworkID: "v35wmpwd0u1azy7bpz1g2av6r",
+                                EndpointID: "97368404b0ef39c64a906ba9311d2d32854b2196305ca7a33aa7581ad4958017",
+                                Gateway: "",
+                                IPAddress: "10.0.1.6",
+                                IPPrefixLen: 24,
+                                IPv6Gateway: "",
+                                GlobalIPv6Address: "",
+                                GlobalIPv6PrefixLen: 0,
+                                MacAddress: "02:42:0a:00:01:06",
+                                DriverOpts: null
+                            }
+                        }
+                    },
+                    Mounts: []
+                },
                 stats: {
                     memory_stats: {
                         stats: {
