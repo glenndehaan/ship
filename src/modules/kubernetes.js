193a4fac39ab9084ad32fddfd569a01e0572a4a1
Glenn de Haan <glenn@dehaan.cloud>
2022-09-17 09:57:56 +0200

    Implemented the kubernetes deployment overview. Also fixed the stats

--- src/modules/kubernetes.js
+++ src/modules/kubernetes.js
@@ -84,44 +84,27 @@ const kubernetesModule = {
 
             resolve(deployments.filter((item) => {
                 return item.metadata.name.includes(search);
-            }).sort((a, b) => a.metadata.name.localeCompare(b.metadata.name)));
+            }).sort((a, b) => a.metadata.namespace.localeCompare(b.metadata.namespace)));
         });
     },
 
     /**
      * Get all kubernetes services
      *
-     * @param search
-     * @param getAll
      * @returns {Promise<unknown>}
      */
-    getServices: (search = '', getAll = false) => {
+    getServices: () => {
         return new Promise(async (resolve) => {
             const allServices = await kubernetes.api.v1.services.get().catch((e) => {
                 console.error(e);
                 process.exit(1);
             });
 
-            let services = allServices.body.items;
+            const services = allServices.body.items.filter((service) => {
+                return !hiddenServices.includes(service.metadata.name);
+            });
 
-            if(!getAll) {
-                services = allServices.body.items.filter((service) => {
-                    return !hiddenServices.includes(service.metadata.name);
-                });
-            }
-
-            // for(let item = 0; item < services.length; item++) {
-            //     const service = services[item];
-            //
-            //     services[item].__tasks = await docker.listTasks({filters: {service: [service.Spec.Name]}}).catch((e) => {
-            //         console.error(e);
-            //         process.exit(1);
-            //     });
-            // }
-
-            resolve(services.filter((item) => {
-                return item.metadata.name.includes(search);
-            }).sort((a, b) => a.metadata.name.localeCompare(b.metadata.name)));
+            resolve(services.sort((a, b) => a.metadata.namespace.localeCompare(b.metadata.namespace)));
         });
     },
 
