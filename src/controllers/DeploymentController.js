4598eabda69e33f3c861d40dd4820c9714cf362b
Glenn de Haan <glenn@dehaan.cloud>
2022-09-21 23:01:16 +0200

    Added deployment action drawers

--- src/controllers/DeploymentController.js
+++ src/controllers/DeploymentController.js
@@ -8,6 +8,7 @@ const kubernetes = require('../modules/kubernetes');
  * Import own utils
  */
 const pageVariables = require('../utils/pageVariables');
+const registry = require("../modules/registry");
 
 /**
  * Define global variables
@@ -52,6 +53,152 @@ module.exports = (app) => {
             deployment_logs: db.getData('/logs').filter((item) => {
                 return item.service === req.params.deployment;
             })
+        });
+    });
+
+    /**
+     * GET /deployment/:namespace/:deployment/update - Update Deployment Drawer (Kubernetes Only)
+     */
+    app.get('/deployment/:namespace/:deployment/update', async (req, res) => {
+        if(!use_kubernetes) {
+            res.status(404);
+            res.render('404', {
+                ...await pageVariables(req),
+                page_title: `Not Found`
+            });
+            return;
+        }
+
+        const deployment = await kubernetes.getDeployment(req.params.namespace, req.params.deployment);
+
+        if(typeof deployment.spec === "undefined") {
+            res.status(404);
+            res.render('404', {
+                ...await pageVariables(req),
+                page_title: `Not Found`
+            });
+            return;
+        }
+
+        res.render('deployment', {
+            ...await pageVariables(req),
+            page_title: `Edit Deployment: ${req.params.namespace}/${req.params.deployment}`,
+            deployment,
+            deployment_logs: db.getData('/logs').filter((item) => {
+                return item.service === req.params.deployment;
+            }),
+            edit: true,
+            edit_service: deployment,
+            edit_service_name: `${req.params.namespace}/${req.params.deployment}`,
+            edit_service_image_tags: await registry.getImageTags(deployment.spec.template.spec.containers[0].image.split(':')[0])
+        });
+    });
+
+    /**
+     * GET /deployment/:namespace/:deployment/force_update - Force Update Deployment Drawer (Kubernetes Only)
+     */
+    app.get('/deployment/:namespace/:deployment/force_update', async (req, res) => {
+        if(!use_kubernetes) {
+            res.status(404);
+            res.render('404', {
+                ...await pageVariables(req),
+                page_title: `Not Found`
+            });
+            return;
+        }
+
+        const deployment = await kubernetes.getDeployment(req.params.namespace, req.params.deployment);
+
+        if(typeof deployment.spec === "undefined") {
+            res.status(404);
+            res.render('404', {
+                ...await pageVariables(req),
+                page_title: `Not Found`
+            });
+            return;
+        }
+
+        res.render('deployment', {
+            ...await pageVariables(req),
+            page_title: `Rollout Restart Deployment: ${req.params.namespace}/${req.params.deployment}`,
+            deployment,
+            deployment_logs: db.getData('/logs').filter((item) => {
+                return item.service === req.params.deployment;
+            }),
+            force_update: true,
+            force_update_service: deployment
+        });
+    });
+
+    /**
+     * GET /deployment/:namespace/:deployment/scale - Scale Deployment Drawer (Kubernetes Only)
+     */
+    app.get('/deployment/:namespace/:deployment/scale', async (req, res) => {
+        if(!use_kubernetes) {
+            res.status(404);
+            res.render('404', {
+                ...await pageVariables(req),
+                page_title: `Not Found`
+            });
+            return;
+        }
+
+        const deployment = await kubernetes.getDeployment(req.params.namespace, req.params.deployment);
+
+        if(typeof deployment.spec === "undefined") {
+            res.status(404);
+            res.render('404', {
+                ...await pageVariables(req),
+                page_title: `Not Found`
+            });
+            return;
+        }
+
+        res.render('deployment', {
+            ...await pageVariables(req),
+            page_title: `Scale Deployment: ${req.params.namespace}/${req.params.deployment}`,
+            deployment,
+            deployment_logs: db.getData('/logs').filter((item) => {
+                return item.service === req.params.deployment;
+            }),
+            scale: true,
+            scale_service: deployment
+        });
+    });
+
+    /**
+     * GET /deployment/:namespace/:deployment/restore - Restore Deployment Drawer (Kubernetes Only)
+     */
+    app.get('/deployment/:namespace/:deployment/restore', async (req, res) => {
+        if(!use_kubernetes) {
+            res.status(404);
+            res.render('404', {
+                ...await pageVariables(req),
+                page_title: `Not Found`
+            });
+            return;
+        }
+
+        const deployment = await kubernetes.getDeployment(req.params.namespace, req.params.deployment);
+
+        if(typeof deployment.spec === "undefined") {
+            res.status(404);
+            res.render('404', {
+                ...await pageVariables(req),
+                page_title: `Not Found`
+            });
+            return;
+        }
+
+        res.render('deployment', {
+            ...await pageVariables(req),
+            page_title: `Rollback Deployment: ${req.params.namespace}/${req.params.deployment}`,
+            deployment,
+            deployment_logs: db.getData('/logs').filter((item) => {
+                return item.service === req.params.deployment;
+            }),
+            restore: true,
+            restore_service: deployment
         });
     });
 }
